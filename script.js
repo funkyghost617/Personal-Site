9ed5f2e5ab69beb0f9453f0aea5d366bd2884e6f
funkyghost617 <112520290+funkyghost617@users.noreply.github.com>
2025-12-18 10:56:38 -0600

    stagger gif start times

--- script.js
+++ script.js
@@ -3,16 +3,18 @@ const numStars = 100;
 
 for (let i = 0; i < numStars; i++) {
     let star = document.createElement("img");
-    if (i < 10) {
+    star.style.position = "absolute";
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.width = `${Math.random() * 20 + 20}px`;
+    container.appendChild(star);
+    setTimeout(function() {
+        if (i < 10) {
         star.src = "miniStar2.gif";
     } else if (i < 55) {
         star.src = "miniStar1.gif";
     } else {
         star.src = "miniStar3.gif";
     }
-    star.style.position = "absolute";
-    star.style.left = `${Math.random() * 100}%`;
-    star.style.top = `${Math.random() * 100}%`;
-    star.style.width = `${Math.random() * 20 + 20}px`;
-    container.appendChild(star);
+    }, Math.floor(Math.random() * 300));
 }
