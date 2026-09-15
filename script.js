e4ddff77948be1d888c3f738559838f0a173a379
funkyghost617 <viplawnboy@gmail.com>
2026-09-15 11:09:25 -0400

    initial restyling

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-const container = document.querySelector(".star-background");
-const numStars = 80;
+let darkmode = document.cookie;
+const themeSwitch = document.getElementById("theme-switch");
 
-for (let i = 0; i < numStars; i++) {
-    let star = document.createElement("img");
-    star.style.position = "absolute";
-    star.style.left = `${Math.random() * 100}%`;
-    star.style.top = `${Math.random() * 100}%`;
-    star.style.width = `${Math.random() * 20 + 20}px`;
-    star.style.opacity = `${Math.random() * 0.6 + 0.2}`;
-    if (i < 8) {
-        star.src = "./media/miniStar2.gif";
-    } else if (i < 45) {
-        star.src = "./media/miniStar1.gif";
-    } else {
-        star.src = "./media/miniStar3.gif";
-    }
-    container.appendChild(star);
+const enableDarkmode = () => {
+  document.body.classList.add("darkmode");
+  document.cookie = "darkmode=active";
+  darkmode = document.cookie;
 }
+
+const disableDarkmode = () => {
+  document.body.classList.remove("darkmode");
+  document.cookie = "darkmode=inactive";
+  darkmode = document.cookie;
+}
+
+if (darkmode == "") disableDarkmode();
+if (darkmode == "darkmode=active") enableDarkmode();
+
+themeSwitch.addEventListener("click", () => {
+  darkmode == "darkmode=inactive" ? enableDarkmode() : disableDarkmode();
+})
